bea0b94f81d06725777ed30ee3c994182eab9910
tohid_bhati <122770307+tohidbhati@users.noreply.github.com>
2026-07-26 20:28:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const buttons = document.querySelectorAll("button");
+const mode = document.getElementById("mode");
+
+// buttons.forEach(button => {
+
+//     button.addEventListener("click", () => {
+
+//         alert("Blog page coming soon!");
+
+//     });
+
+// });
+
+mode.addEventListener("click", function()
+{
+    
+})
